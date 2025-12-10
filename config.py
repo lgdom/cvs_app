66d7ac18708106979829c501c89199bcf0d7ab98
lgdom <66961915+lgdom@users.noreply.github.com>
2025-12-10 15:18:07 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+# --- CONFIGURACIÓN DE PÁGINA ---
+PAGE_TITLE = "Sistema Ventas"
+PAGE_ICON = "💊"
+LAYOUT = "wide"
+
+# --- ARCHIVOS ---
+FILE_CLIENTES = 'clientes.csv'
+FILE_PRODUCTOS = 'productos.csv'
+FILE_PLANTILLA = 'plantilla.xlsx'
+FILE_IMAGEN = 'logo.png'
+
+# --- DRIVE ---
+DRIVE_FOLDER_ID = "1bvF7yuIRiJQ0oiXiZ6s3JD8goy1DUi1K"
+TEMP_DRIVE_FOLDER = './temp_drive_folder'
+
+# --- ZONA HORARIA ---
+TIMEZONE = 'America/Tijuana'
